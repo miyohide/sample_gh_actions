19e17cd843820024f3bf93ae5a2f3b5e96c9036b
Hidenori Miyoshi <miyohide@gmail.com>
2019-11-17 16:44:25 +0900

    fix fatal issue that throws could not read Username

--- commit.sh
+++ commit.sh
@@ -3,6 +3,8 @@
 git config --global user.name "Hidenori Miyoshi"
 git config --global user.email "miyohide@gmail.com"
 
+git remote set-url origin https://miyohide:${GITHUB_TOKEN}@github.com/miyohide/sample_gh_actions.git
+
 git checkout -b master
 
 git add .
